28a5c567f5d8045cfb30fee1c8c57065285e27ca
unknown <1157363323@qq.com>
2025-03-01 18:44:59 +0100

    modify the predict funtion

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,47 @@
 # Flask backend routes
-from flask import Flask, render_template, request
+from flask import render_template, request
+from ml import predict 
+from app import app  # Import Flask app from __init__.py
 
-app = Flask(__name__)
+
+integer_column = ["Age", "Credit Score", "Previous Defaults", "Years at Current Job"]
+float_column = ["Income", "Debt-to-Income Ratio", "Loan Amount"]
+str_colun = ["Payment History", "Employment Status"]
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/input')
+def input_page():
+    return render_template('input.html')
+
+@app.route('/predict', methods=['POST'])
+def predict_loan_approval():
+    #features = [float(request.form[x]) for x in important_columns]
+
+    request_data = request.form  # dict[str, str]
+    features_dict = {}
+    
+    for k, v in request_data.items():
+        if k in integer_column:
+            features_dict[k] = int(v)
+        if k in float_column:
+            features_dict[k] = float(v)
+        if k in str_colun:
+            features_dict[k] = str(v)
+        
+    # Make prediction
+    prediction = predict.predict(features_dict)
+
+    # Interpret prediciton
+    result = ''
+    if prediction == 0:
+        result = 'Approved'
+    else:
+        result = 'Rejected'
+
+    return render_template('result.html', prediction_text=f'The client is {result} for the loan application')
+
+#if __name__ == '__main__':
+#    app.run(debug=True)
--- ml/predict.py
+++ ml/predict.py
@@ -1,3 +1,40 @@
-# Prediction code
-def predict(data):
-    return 'Prediction result'
+import pickle
+import numpy as np
+
+ordered_columns = [
+    "Age", "Income", "Credit Score", "Debt-to-Income Ratio", "Loan Amount",
+    "Payment History", "Previous Defaults", "Employment Status", "Years at Current Job"
+]
+
+
+sample_data = {
+    "Age": 30,
+    "Income": 50000,
+    "Credit Score": 700,
+    "Debt-to-Income Ratio": 0.3,
+    "Loan Amount": 20000,
+    "Payment History": 'Excellent',
+    "Previous Defaults": 0,
+    "Employment Status": "Employed",
+    "Years at Current Job": 5
+}
+
+
+def predict(data: dict):
+    # Load the prediction model
+    model_name = 'best_prediction.pkl'
+    with open(model_name, 'rb') as file:
+        model = pickle.load(file)
+
+    # features need one-hot encoded as it contains non-numeric values.
+    features = [data[x] for x in ordered_columns]
+
+    # transform features
+    final_features = features
+
+    # reshape final_features
+    final_features = np.array(final_features).reshape(1, -1)
+    prediction = model.predict(final_features)
+
+    return prediction
+
