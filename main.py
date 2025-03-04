e1cb39fb1998ce67c005c289184a7750bf41572a
unknown <1157363323@qq.com>
2025-03-04 20:50:51 +0100

    add a browser open operation

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import webbrowser
 # Flask entry point
 from app import app  # Import Flask app from __init__.py
 
 if __name__ == '__main__':
+    # Open the default web browser to the homepage
+    webbrowser.open("http://127.0.0.1:5000/index")
+    
+    # Start the Flask server
     app.run(host='127.0.0.1', port=5000, debug=True)
